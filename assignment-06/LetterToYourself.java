a419605a306aa6071ab62b3f0c8a4b76482c21a8
negat1v <spirit1309@gmail.com>
2016-03-14 23:23:25 +0300

    complete Basics and Printing

---
+++ assignment-06/LetterToYourself.java
@@ -0,0 +1,20 @@
+class LetterToYourself
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "+---------------------------------------------+" );
+		System.out.println( "|                                     #####   |" );
+		System.out.println( "|                                     #####   |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "|                               for Skanatek  |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "|                                             |" );
+		System.out.println( "+---------------------------------------------+" );
+
+	}
+}
---
+++ assignment-07/YourInitials.java
@@ -0,0 +1,13 @@
+class YourInitials
+{
+	public static void main( String[] args )
+	{
+		System.out.println(" H     H  E E E E  L        L        O O O O ");
+		System.out.println(" H     H  E        L        L        O     O ");
+		System.out.println(" H     H  E        L        L        O     O ");
+		System.out.println(" H H H H  E E E E  L        L        O     O ");
+		System.out.println(" H     H  E        L        L        O     O ");
+		System.out.println(" H     H  E        L        L        O     O ");
+		System.out.println(" H     H  E E E E  L L L L  L L L L  O O O O ");
+	}
+}
