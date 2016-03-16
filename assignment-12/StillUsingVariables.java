e52ef8b6ea31b96e210015f5356b26295502b2d3
negat1v <spirit1309@gmail.com>
2016-03-16 20:16:28 +0300

    assignment - 12

---
+++ assignment-12/StillUsingVariables.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+
+public class StillUsingVariables
+{
+	public static void main( String[] args ) throws java.io.IOException 
+		 {
+		 	String name;
+		 	int year;
+		 	
+		 	name = "Philipp Pushkov";
+
+		 	year = 2015;
+
+			System.out.println(" My name is " + name + " and I'll graduate in " + year);
+
+		 }
+
+	
+}
